c3b06f9cd651132f466785340b5440142b5bd536
Henrique Gogó <henriquegogo@gmail.com>
2021-10-26 00:29:29 -0300

    Using fastapi instead of flask

--- main.py
+++ main.py
@@ -1,34 +1,22 @@
-from flask import Flask, Response, jsonify, request
+from fastapi import Body, FastAPI
+from uvicorn import run # type: ignore
 from plyvel import DB # type: ignore
+import json
 
-db: DB = DB('database', create_if_missing=True)
-app: Flask = Flask(__name__)
+db = DB('database', create_if_missing=True)
+app: FastAPI = FastAPI()
 
-@app.get('/')
-def root() -> Response:
-    return Response(status=200)
+@app.get('/{prefix}')
+def read(prefix):
+    return [json.loads(value.decode()) for _, value in db.iterator(prefix=prefix.encode())]
 
-@app.get('/<prefix>')
-def read(prefix: str) -> Response:
-    items = []
+@app.post('/{id}')
+def write(id, body = Body(None)):
+    return db.put(id.encode(), json.dumps(body).encode())
 
-    for _, value in db.iterator(prefix=prefix.encode()):
-        items.append(value.decode())
-
-    return jsonify(items)
-
-@app.post('/<id>')
-@app.put('/<id>')
-def write(id: str) -> Response:
-    db.put(id.encode(), str(request.json).encode())
-
-    return Response(status=200)
-
-@app.delete('/<id>')
-def delete(id: str) -> Response:
-    db.delete(id.encode())
-
-    return Response(status=200)
+@app.delete('/{id}')
+def delete(id):
+    return db.delete(id.encode())
 
 if __name__ == '__main__':
-    app.run()
+    run('__main__:app')
