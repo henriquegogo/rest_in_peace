beb3b676b4b3043a0214857e342fe551bae620b6
Henrique Gogó <henriquegogo@gmail.com>
2021-10-26 01:11:16 -0300

    CRUD operations as a simple way

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 from fastapi import Body, FastAPI
 from uvicorn import run # type: ignore
 from plyvel import DB # type: ignore
+from uuid import uuid4
 import json
 
 db = DB('database', create_if_missing=True)
 app: FastAPI = FastAPI()
 
+@app.get('/')
+def root():
+    return [key.decode() for key, _ in db.iterator()]
+
 @app.get('/{prefix}')
-def read(prefix):
+def list(prefix):
     return [json.loads(value.decode()) for _, value in db.iterator(prefix=prefix.encode())]
 
-@app.post('/{id}')
-def write(id, body = Body(None)):
-    return db.put(id.encode(), json.dumps(body).encode())
+@app.get('/{prefix}/{id}')
+def read(prefix, id):
+    return json.loads(db.get((prefix + '/' + id).encode()))
+
+@app.post('/')
+@app.post('/{prefix}')
+def write(prefix = None, body = Body(None)):
+    body['id'] = str(uuid4())
+    key = prefix + '/' + body['id'] if prefix else body['id']
+    db.put(key.encode(), json.dumps(body).encode())
+    return
+
+@app.put('/{id}')
+@app.put('/{prefix}/{id}')
+def update(id, prefix = None, body = Body(None)):
+    key = prefix + '/' + id if prefix else id
+    db.put(key.encode(), json.dumps(body).encode())
+    return
 
 @app.delete('/{id}')
-def delete(id):
-    return db.delete(id.encode())
+@app.delete('/{prefix}/{id}')
+def delete(id, prefix = None):
+    key = prefix + '/' + id if prefix else id
+    db.delete(key.encode())
+    return
 
 if __name__ == '__main__':
     run('__main__:app')
