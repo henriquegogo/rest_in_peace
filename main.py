6dea197b1a3cca8d62a755d821eccb16c95a2185
Henrique Gogó <henriquegogo@gmail.com>
2021-10-29 02:08:30 -0300

    Use sqlite instead of leveldb

--- main.py
+++ main.py
@@ -1,41 +1,72 @@
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, HTTPException
 from uvicorn import run # type: ignore
-from plyvel import DB # type: ignore
-from uuid import uuid4
-import json
+import sqlite3
 
-db = DB('database', create_if_missing=True)
+db_path = 'database.db'
 app = FastAPI()
 
 @app.get('/')
-def root(limit: int = 10, offset: int = 0):
-    return [key.decode() for key, _ in db][offset:offset+limit]
+def root():
+    db = sqlite3.connect(db_path).cursor()
+    return [row[0] for row in
+            db.execute('SELECT name FROM sqlite_master WHERE type="table" AND name NOT LIKE "sqlite_%"')]
 
 @app.get('/{collection}')
-def list(collection: str, limit: int = 10, offset: int = 0):
-    return [json.loads(value.decode())
-            for _, value in db.iterator(prefix=(collection + '/').encode())
-            ][offset:offset+limit]
+def list(collection: str):
+    db = sqlite3.connect(db_path).cursor()
+    try:
+        schema = [row[1] for row in db.execute(f'PRAGMA table_info({collection})')]
+        return [zip(schema, row) for row in db.execute(f'SELECT * FROM {collection}')]
+    except: raise HTTPException(status_code=404)
 
 @app.post('/{collection}')
 def create(collection: str, body: dict = Body(None)):
-    body['id'] = str(uuid4())
-    key = collection + '/' + body['id']
-    return db.put(key.encode(), json.dumps(body).encode())
+    connection = sqlite3.connect(db_path)
+    db = connection.cursor()
+    try: db.execute(f'CREATE TABLE {collection} (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE)')
+    except: pass
+    try: [db.execute(f'ALTER TABLE {collection} ADD COLUMN {key} TEXT') for key in body.keys()]
+    except: pass
+    try:
+        keys = ', '.join([key for key in body.keys()])
+        values = str(tuple([value for value in body.values()]))
+        db.execute(f'INSERT INTO {collection} ({keys}) VALUES {values}')
+        connection.commit()
+        return HTTPException(status_code=201)
+    except: raise HTTPException(status_code=400)
 
 @app.get('/{collection}/{id}')
 def read(collection: str, id: str):
-    return json.loads(db.get((collection + '/' + id).encode()))
+    db = sqlite3.connect(db_path).cursor()
+    try:
+        schema = [row[1] for row in db.execute(f'PRAGMA table_info({collection})')]
+        return [zip(schema, row) for row in db.execute(f'SELECT * FROM {collection} WHERE id = ?', id)][0]
+    except: raise HTTPException(status_code=404)
 
+#WIP
 @app.put('/{collection}/{id}')
 def update(collection: str, id: str, body: dict = Body(None)):
-    key = collection + '/' + id
-    return db.put(key.encode(), json.dumps(body).encode())
+    connection = sqlite3.connect(db_path)
+    db = connection.cursor()
+    try: db.execute(f'CREATE TABLE {collection} (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE)')
+    except: pass
+    try: [db.execute(f'ALTER TABLE {collection} ADD COLUMN {key} TEXT') for key in body.keys()]
+    except: pass
+    try:
+        [db.execute(f'UPDATE {collection} SET {key} = "{value}" WHERE id = ?', id) for key, value in body.items()]
+        connection.commit()
+        return HTTPException(status_code=200)
+    except: raise HTTPException(status_code=400)
 
 @app.delete('/{collection}/{id}')
 def delete(collection: str, id: str):
-    key = collection + '/' + id
-    return db.delete(key.encode())
+    connection = sqlite3.connect(db_path)
+    db = connection.cursor()
+    try:
+        db.execute(f'DELETE FROM {collection} WHERE id = ?', id)
+        connection.commit()
+        return HTTPException(status_code=200)
+    except: raise HTTPException(status_code=404)
 
 if __name__ == '__main__':
     run('__main__:app')
